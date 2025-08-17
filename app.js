c8e929d4862fb31a66235744488200d83942468f
Aabid Ali <aabidali@gmail.com>
2025-08-18 01:58:52 +0530

    file update

---
+++ app.js
@@ -0,0 +1,7 @@
+
+let h1 = document.querySelector("h1");
+h1.addEventListener("click",() => {
+    console.log("this is click")
+    h1.style.color = "blue";
+    h1.style.backgroundColor = "green";
+});
